e67aad1d32c686f53ba06ad1735def6b38e5e3b4
Chris Williams <chriscancodethat@gmail.com>
2023-08-01 15:14:37 -0400

    prompt updates

--- scripts/deckbuilder.js
+++ scripts/deckbuilder.js
@@ -1,30 +1,30 @@
-async function _handleBuildDeckSkill(card) {
-	console.log("GOT IT");
-	const context = {
-		scryfallApiQueryString: card.value,
-	};
-	const model = window.models.CreateModel("deckbuilder:scryfallapi");
-	window.models.ApplyContextObject(model, context);
-	await window.models.CallModel(model);
-	window.models.DestroyModel(model);
-}
+/**
+ * The Deckbuilder Module
+ * Gives the companion the ability to fetch Magic: The Gathering card information from Scryfall
+ */
 
-function _handleApiResponse({ response }) {
-	const responseObj = JSON.parse(response);
-	const name = window.companion.GetCharacterAttribute("name");
-	console.log(responseObj);
-	const card = JSON.parse(response).name;
-	window.hooks.emit("moemate_core:handle_skill_text", {
-		name: name,
-		value: card,
-	});
+async function _handleFetchCardSkill(keywords) {
+	console.log('got it', keywords)
+    const context = {
+        cardName: keywords.value
+    }
+    const model = window.models.CreateModel('deckbuilder:scryfallapi')
+    window.models.ApplyContextObject(model, context);
+    await window.models.CallModel(model);
+    window.models.DestroyModel(model);
 }
+ 
+function _handleApiResponse({response}) {
+    const responseObj = JSON.parse(response);
+    const name = window.companion.GetCharacterAttribute('name');
 
+    // Send card details
+    const cardDetails = `Card Name: ${responseObj.name}\nType: ${responseObj.type_line}\nOracle Text: ${responseObj.oracle_text}`;
+    window.hooks.emit('moemate_core:handle_skill_text', {name: name, value: cardDetails});
+}
+ 
 export function init() {
-	console.log("init deck builder");
-	window.hooks.on("deckbuilder:handle_find_card_skill", _handleBuildDeckSkill);
-	window.hooks.on(
-		"models:response:deckbuilder:scryfallapi",
-		_handleApiResponse
-	);
+	console.log('init new deckbuilder')
+    window.hooks.on('deckbuilder:handle_fetch_card_skill', _handleFetchCardSkill)
+    window.hooks.on('models:response:deckbuilder:scryfallapi', _handleApiResponse)
 }
