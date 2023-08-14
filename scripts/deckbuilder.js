1d84104126e5b41d396e45b267e84b003dcb81c6
Chris Williams <chriscancodethat@gmail.com>
2023-08-14 18:27:06 -0400

    update skill to use hack features

--- scripts/deckbuilder.js
+++ scripts/deckbuilder.js
@@ -3,28 +3,57 @@
  * Gives the companion the ability to fetch Magic: The Gathering card information from Scryfall
  */
 
-async function _handleFetchCardSkill(keywords) {
-	console.log('got it', keywords)
+async function _handleFetchCardSkill(event) {
+    window.companion.SendMessage({type: "FETCH_CARD", user: event.name, value: event.value});
+    console.log(event)
     const context = {
-        cardName: keywords.value
+        cardName: event.value
     }
+
     const model = window.models.CreateModel('deckbuilder:scryfallapi')
     window.models.ApplyContextObject(model, context);
     await window.models.CallModel(model);
     window.models.DestroyModel(model);
 }
- 
-function _handleApiResponse({response}) {
-    const responseObj = JSON.parse(response);
-    const name = window.companion.GetCharacterAttribute('name');
 
-    // Send card details
-    const cardDetails = `Card Name: ${responseObj.name}\nType: ${responseObj.type_line}\nOracle Text: ${responseObj.oracle_text}`;
-    window.hooks.emit('moemate_core:handle_skill_text', {name: name, value: cardDetails});
+function summarizeCard(card) {
+  const name = card.name;
+  const manaCost = card.mana_cost;
+  const typeLine = card.type_line;
+  const oracleText = card.oracle_text;
+  const set = card.set_name;
+  const artist = card.artist_ids && card.artist_ids[0] ? card.artist_ids[0] : 'Unknown Artist';
+  const rarity = card.rarity;
+  const legalities = Object.keys(card.legalities)
+    .filter(key => card.legalities[key] === 'banned')
+    .join(', ');
+
+  return {
+    name,
+    manaCost,
+    typeLine,
+    oracleText,
+    set,
+    artist,
+    rarity,
+    legalities
+  }
 }
- 
+
+function _handleApiResponse(response) {
+  if (response.response) {
+    const summary = summarizeCard(response.response);
+    console.log(summary.name, summary.manaCost, summary.oracleText)
+    // Send card details
+    window.hooks.emit('moemate_core:handle_skill_text', {name: name, value: `${summary.name}, ${summary.manaCost}, ${summary.oracleText}`});
+    // window.hooks.emit("hack_delay", `Role play that you found information about this card by querying the Scryfall API for me:\nName:${summary.name}\nMana Cost:${summary.manaCost}\nOracle Text:${summary.oracleText}-Avoid thanking me, Avoid saying I found them or shared them with you, Avoid giving any other information except for the information provided. Pretend this was all your doing`)
+  } else {
+    // window.hooks.emit("hack_delay", `Role play that you had trouble looking up information about this card by querying the Scryfall API\n-Ask me to try again later.`)
+  }
+}
+
 export function init() {
-	console.log('init new deckbuilder')
-    window.hooks.on('deckbuilder:handle_fetch_card_skill', _handleFetchCardSkill)
-    window.hooks.on('models:response:deckbuilder:scryfallapi', _handleApiResponse)
+	console.log('init deckbuilder')
+  window.hooks.on('deckbuilder:handle_fetch_card_skill', _handleFetchCardSkill)
+  window.hooks.on('models:response:deckbuilder:scryfallapi', _handleApiResponse)
 }
