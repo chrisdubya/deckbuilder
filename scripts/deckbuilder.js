e2466f9b0911cf05dd1ac92d932891901941c7d9
Chris Williams <chriscancodethat@gmail.com>
2023-08-16 18:56:42 -0400

    working version with scryfall api

--- scripts/deckbuilder.js
+++ scripts/deckbuilder.js
@@ -5,18 +5,18 @@
 
 async function _handleFetchCardSkill(event) {
     window.companion.SendMessage({type: "FETCH_CARD", user: event.name, value: event.value});
-    console.log(event)
     const context = {
         cardName: event.value
     }
 
-    const model = window.models.CreateModel('deckbuilder:scryfallapi')
+    const model = window.models.CreateModel('deckbuilder:deckbuilderapi')
     window.models.ApplyContextObject(model, context);
     await window.models.CallModel(model);
     window.models.DestroyModel(model);
 }
 
 function summarizeCard(card) {
+  console.log(card)
   const name = card.name;
   const manaCost = card.mana_cost;
   const typeLine = card.type_line;
@@ -43,17 +43,19 @@ function summarizeCard(card) {
 function _handleApiResponse(response) {
   if (response.response) {
     const summary = summarizeCard(response.response);
-    console.log(summary.name, summary.manaCost, summary.oracleText)
+    console.log(summary)
     // Send card details
-    window.hooks.emit('moemate_core:handle_skill_text', {name: name, value: `${summary.name}, ${summary.manaCost}, ${summary.oracleText}`});
-    // window.hooks.emit("hack_delay", `Role play that you found information about this card by querying the Scryfall API for me:\nName:${summary.name}\nMana Cost:${summary.manaCost}\nOracle Text:${summary.oracleText}-Avoid thanking me, Avoid saying I found them or shared them with you, Avoid giving any other information except for the information provided. Pretend this was all your doing`)
+    // window.hooks.emit('moemate_core:handle_skill_text', {name: name, value: `${summary.name}, ${summary.manaCost}, ${summary.oracleText}`});
+    setTimeout(() => {
+      window.hooks.emit("hack_delay", `Role play that you found information about this card by querying the Scryfall API for me:\nName:${summary.name}\nMana Cost:${summary.manaCost}\nOracle Text:${summary.oracleText}\nLegalities: ${summary.rarity}-Avoid thanking me, Avoid saying I found them or shared them with you, Avoid giving any other information except for the information provided. Pretend this was all your doing`)
+    }, 1000)
   } else {
-    // window.hooks.emit("hack_delay", `Role play that you had trouble looking up information about this card by querying the Scryfall API\n-Ask me to try again later.`)
+    window.hooks.emit("hack_delay", `Role play that you had trouble looking up information about this card by querying the Scryfall API\n-Ask me to try again later.`)
   }
 }
 
 export function init() {
 	console.log('init deckbuilder')
   window.hooks.on('deckbuilder:handle_fetch_card_skill', _handleFetchCardSkill)
-  window.hooks.on('models:response:deckbuilder:scryfallapi', _handleApiResponse)
+  window.hooks.on('models:response:deckbuilder:deckbuilderapi', _handleApiResponse)
 }
